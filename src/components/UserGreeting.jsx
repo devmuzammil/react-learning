8729e0a5bab4bc9a38e4b5c47141bad4d65343d6
Muzammil Hussain <144897920+devmuzammil@users.noreply.github.com>
2025-04-30 16:37:26 +0500

    Update UserGreeting.jsx

--- src/components/UserGreeting.jsx
+++ src/components/UserGreeting.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 function UserGreeting(props) {
 
     const welcomeMessage = <h2>Welcome {props.username}</h2>;
-    const loginPrompt = <h2>Login kar</h2>
+    const loginPrompt = <h2>Login</h2>
     return (props.isloggedIn ? welcomeMessage : loginPrompt)
 }
 
